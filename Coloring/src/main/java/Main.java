244b51b9582ede944664d868b216645e9b1e10d8
Tymofii Chudakov <tymofii.chudakov@gmail.com>
2020-04-26 16:33:15 +0000

    More changes

--- Coloring/src/main/java/Main.java
+++ Coloring/src/main/java/Main.java
@@ -13,30 +13,43 @@ public class Main {
         task curtask = new task();
         curtask.addJarFile("Coloring.jar");
 
+	System.out.println("Stage = 1");
+
         AMInfo info = new AMInfo(curtask, null);
         point p = info.createPoint();
         channel c = p.createChannel();
         p.execute("Runner");
+	
+	System.out.println("Stage = 2");
 
         DefaultUndirectedGraph<Integer, DefaultEdge> graph = new DefaultUndirectedGraph<>(DefaultEdge.class);
         Graphs.addEdgeWithVertices(graph, 1, 2);
         Graphs.addEdgeWithVertices(graph, 1, 3);
         Graphs.addEdgeWithVertices(graph, 2, 3);
 
+	System.out.println("Stage = 3");
+
         HashMap<Integer, Integer> colorMap = new HashMap<>();
         c.write(graph);
         c.write(colorMap);
         c.write(1);
-        // System.out.printf("Sent number %d\n", number);
+
+
+        System.out.printf("Graph = %s\n", graph);
         System.out.println("Waiting for result...");
 
         long start = System.nanoTime();
-
-        Map<Integer, Integer> resultColoring = (HashMap<Integer, Integer>) c.readObject();
-        System.out.println("Received final coloring");
-        for (Map.Entry<Integer, Integer> color : resultColoring.entrySet()) {
-            System.out.printf("%d -> %d\n", color.getKey(), color.getValue());
-        }
+	
+	int code = c.readInt();
+	if(code == 0){
+		System.out.println("No coloring available (strange)");
+	} else {
+        	Map<Integer, Integer> resultColoring = (HashMap<Integer, Integer>) c.readObject();
+        	System.out.println("Received final coloring");
+        	for (Map.Entry<Integer, Integer> color : resultColoring.entrySet()) {
+           		System.out.printf("%d -> %d\n", color.getKey(), color.getValue());
+        	}
+	}
 
         long end = System.nanoTime();
 
